90df08b4616090be5849b2c254fb256426229e3f
Rafal Enden <git@enden.com>
2025-06-24 12:52:39 +0200

    Add city to analytics

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,24 @@ export default {
       return new Response(`No URL mapped for ‘${slug}’.`, { status: 404 });
     }
 
+    // @ts-ignore
+    const cfProperties = request.cf;
+
     ctx.waitUntil(
       env.ENDEN_LINK_VIEWS.writeDataPoint({
-        doubles: [1],
         blobs: [
           slug,
           dest,
           request.headers.get('user-agent'),
-          // @ts-ignore
-          request.cf?.country,
+          request.headers.get('referer'),
+          cfProperties.city as string,
+          cfProperties.country as string,
+          cfProperties.continent as string,
+          cfProperties.region as string,
+          cfProperties.regionCode as string,
+          cfProperties.timezone as string,
         ],
+        doubles: [cfProperties.metroCode as number, cfProperties.longitude as number, cfProperties.latitude as number],
         indexes: [slug],
       })
     );
