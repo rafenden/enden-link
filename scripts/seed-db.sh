f81308625940fade81afc5d5cb8f8e7fddd1d974
Rafal Enden <git@enden.com>
2025-07-06 22:21:33 +0200

    Update seed-db script

--- scripts/seed-db.sh
+++ scripts/seed-db.sh
@@ -1,5 +1,5 @@
 wrangler kv namespace create ENDEN_LINK_URLS --remote
-wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "linkedin" "https://www.linkedin.com/in/rafenden/"
-wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "stackoverflow" "https://stackoverflow.com/users/3042543/rafal-enden"
-wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "soundcloud" "https://soundcloud.com/rafenden"
-wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "from-cv" "https://enden.com"
+wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "linkedin" "url: https://www.linkedin.com/in/rafenden/"
+wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "stackoverflow" "url: https://stackoverflow.com/users/3042543/rafal-enden"
+wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "soundcloud" "url: https://soundcloud.com/rafenden"
+wrangler kv key put --binding ENDEN_LINK_URLS --preview false --remote "from-cv" "url: https://enden.com"
